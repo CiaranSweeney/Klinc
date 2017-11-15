eb27f5556b818afc0508aeb238098f410744ce10
Ciaran <ciaran2293@yahoo.con>
2017-11-15 19:57:41 +0000

    cards are in the right order and you can see the oppononets bomber cards

---
+++ Klinc(Beta)/Assets/ProjectData/Scripts/Ciaran Scripts/Card.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//Ciaran Sweeney 12/10/2017
+//edited by Peter Major 21/10
+//edited by Ciaran Sweeney 28/10
+//edited by Ciaran Sweeney 1/11
+public class Card : MonoBehaviour {
+
+    // Use this for initialization
+	[SerializeField]int cardValue;
+    public Vector2 cardPoistion;
+    public Sprite cardBack;
+    public Sprite bomberCardback;
+    public Sprite cardFace;
+    public Image cardImage;
+
+    bool bomberCard = false;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //cardFace = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+        cardImage = GetComponent<Image>();
+        cardFace = cardImage.sprite;
+    }
+   
+    public void SetBomberCard()
+    {
+        bomberCard = true;
+        gameObject.SetActive(false);
+    }
+    public void SetBomberCardOnTable()
+    {
+        bomberCard = true;
+        cardImage.sprite = bomberCardback;
+        transform.Rotate(0, 0, 90);
+    }
+    
+    public bool IsBomberCard()
+    {
+        return bomberCard;
+    }
+
+    public void SetPosition(Vector2 pos)
+    {
+        cardPoistion = pos;
+        transform.position = cardPoistion;
+    }
+    public int GetCardValue()
+    {
+        return cardValue;
+    }
+
+    public void FaceDown()
+    {
+        cardImage.sprite = cardBack;
+    }
+
+    public void FaceUp()
+    {
+        cardImage.sprite = cardFace;
+        //sr.sprite = cardFace;
+    }
+
+    public void HideCard()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowCard()
+    {
+        gameObject.SetActive(true);
+    }
+}
