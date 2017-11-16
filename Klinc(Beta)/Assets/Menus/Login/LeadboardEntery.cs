c26be1ddf5a24c4b96347cd5f0c70f02f8490511
AngryMajor <peterfmajor@gmail.com>
2017-11-16 21:08:13 +0000

    Add files via upload

---
+++ Klinc(Beta)/Assets/Menus/Login/LeadboardEntery.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeadboardEntery : MonoBehaviour {
+
+	[SerializeField]Text place;
+	[SerializeField]Text playerName;
+	[SerializeField]Text drinksTaken;
+	[SerializeField]Text drinksGiven;
+	[SerializeField]Text ratio;
+
+	public void SetEnteryData(int Place, string Name, int DrinksTaken, int DrinksGiven, int Ratio){
+		place.text = "" +Place;
+		playerName.text = Name;
+		drinksTaken.text = "" + DrinksTaken;
+		drinksGiven.text = "" + DrinksGiven;
+		ratio.text = "" + ratio;
+	}
+
+}
---
+++ Klinc(Beta)/Assets/Menus/Login/LeaderboardControler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class LeaderboardControler : MonoBehaviour {
+
+	[SerializeField]GameObject EnteryPrefab;
+	VerticalLayoutGroup VLG;
+	RectTransform rTransform;
+	ScrollRect sRect;
+	float oldPoss = 0;
+	float moveDelta = 0;
+	public int NumOfEntriesToLoad = 12;
+
+	LeadboardEntery[] BoardArray;
+
+	void Awake(){
+		VLG = gameObject.GetComponent<VerticalLayoutGroup>();
+		rTransform = gameObject.GetComponent<RectTransform>();
+		sRect = transform.parent.GetComponent<ScrollRect>();
+		BoardArray = new LeadboardEntery[NumOfEntriesToLoad];
+		for(int i=0;i < NumOfEntriesToLoad;i++){
+			BoardArray[i] = AddEntryToTop();
+		}
+
+	}
+
+	public void FixedUpdate(){
+		rTransform.sizeDelta = new Vector2(505,VLG.minHeight);
+	}
+
+	public LeadboardEntery AddEnteryToBotom(){
+		GameObject temp = Instantiate(EnteryPrefab,this.transform);
+		return temp.GetComponent<LeadboardEntery>();
+	}
+	public LeadboardEntery AddEntryToTop(){
+		GameObject temp = Instantiate(EnteryPrefab,this.transform);
+		temp.transform.SetAsFirstSibling();
+		return temp.GetComponent<LeadboardEntery>();
+	}
+
+	public void WhenScroling(){
+		moveDelta += rTransform.position.y-oldPoss;
+		oldPoss = rTransform.position.y;
+		if(moveDelta > 71){
+			moveDelta = 0;
+		}else if(moveDelta < 72){
+			moveDelta = 0;
+		}
+		Debug.Log(rTransform.position.y);
+	}
+}
